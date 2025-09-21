89e0681bfe8b8fe09eee9cdc4ab6fa9b0736b6eb
Shivam Mandal <109310628+Shivam-Mandal@users.noreply.github.com>
2025-09-21 19:38:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: ['http://localhost:5173','https://clothflow.onrender.com/'] 
   credentials: true,               
 }));
 app.use(express.json()); // Parse JSON bodies
@@ -37,3 +37,4 @@ connectDB()
         console.error('Database connection failed:', error);
         process.exit(1);
     });
+
