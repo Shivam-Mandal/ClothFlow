06c81ec0d8849c6d0ff0dee80ea5dd36ff3178cb
Shivam Mandal <sanchayetamandal83404@gmail.com>
2025-09-30 14:48:58 +0530

    changed backend api url in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://clothflow-backend.onrender.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://clothflow-backend.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
   withCredentials: true, 
 });
 
